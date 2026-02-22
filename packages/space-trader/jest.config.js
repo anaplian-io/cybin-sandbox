d2d7ec88e4a06d6cbf12edb4990c1ad4e36f59be
Ethan Fortner <ethan@anaplian.io>
2026-02-22 15:15:07 -0500

    revert: remove ts-jest warning suppression

--- packages/space-trader/jest.config.js
+++ packages/space-trader/jest.config.js
@@ -2,9 +2,6 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { diagnostics: { ignoreCodes: [151002] } }],
-  },
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
